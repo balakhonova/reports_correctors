1f64495d4848015908fba9b66656540173afa562
Igor Talkov <ballenoc@mail.ru>
2018-02-20 21:41:15 +0300

    Add files via upload

--- corrector.py
+++ corrector.py
@@ -24,9 +24,11 @@ if len(sys.argv) == 2:
     text = sys.argv[1]
 
 #выношу цены в отдельный словарь для удобства возможных корректировок тарифов
-correct_price = {"Промо":150, "Базовый":250, "Супербазовый":350, "Дождь":240, "ПЛЮС ФУТБОЛ":380, "ПЛЮС КИНО":380, "25 за 25":25,
-                 "Ночной":150, "Промо бандл (Лайт)":100,"Базовый бандл":250, "Супербазовый бандл":350, "Amedia Premium HD":199,
-                 "Наш футбол HD":219, "Наш футбол":219, "Trial":50, "SHANT Premium":240, "Публичный":990}
+
+correct_price = {"Промо":150, "Базовый":250, "Супербазовый":350, "Дождь":240, "МАТЧ! Футбол":380, "Настрой кино":380, "25 за 25":25,
+                 "Ночной":199, "Первый Бандл":50,"Промо Бандл":100,"Базовый Бандл":250, "Супербазовый Бандл":350, "Amedia Premium HD":199,
+                 "Наш футбол HD":219, "Наш футбол":219, "Trial":50, "SHANT Premium":240, "Публичный":990, "Поддержка":0, "Архив Бесплатный":0,
+                 "Старт":0, "Архив Ночной":150, "Поддержка VOD":0,"Супербазовый Бандл 1/3":0, "Промо Бандл 1/3":0, "Базовый Бандл 1/3":0}
         
 #Пройдусь по всем файликам, имеющим расширение xls, в текущей директории и заменю расширение на xlsx
 
@@ -66,10 +68,12 @@ for file in os.listdir('.'):
         for i in range(1,ws.max_column):
             if ws.cell(row=6,column=i).value == "Стоимость подписки":
                 column_price_index = i
+                col_reg = get_column_letter(i-1)
+                wdt = ws.column_dimensions[col_reg].width = 16
         for price_cell in range(6, ws.max_row):
             current_price = ws.cell(row=price_cell ,column=column_price_index).value
             tariff = ws.cell(row=price_cell ,column=column_price_index - 1).value
-            if current_price == 0 and not (tariff == "Поддержка" or tariff == "Старт" or tariff == "Бесплатный (архив)"):
+            if current_price == 0 and not (tariff == "Поддержка" or tariff == "Старт" or tariff == "Архив Бесплатный" or tariff == "Лайт МА"):
                 new_price = correct_price.get(tariff)
                 ws.cell(row=price_cell ,column=column_price_index).value = new_price
                 print("В строке ", ws.cell(row=price_cell,column=1).value, " отчета заменена цена")
@@ -87,7 +91,7 @@ for file in os.listdir('.'):
                 #b = ws.cell(row=ws.max_row-7,column=i).coordinate
                 ws.cell(row=ws.max_row-5,column=i).value = "=SUM("+ ws.cell(row=7,column=i).coordinate +":"+ ws.cell(row=ws.max_row-7,column=i).coordinate +")"
                 ws.cell(row=ws.max_row-5,column=i+1).value = "=SUM("+ ws.cell(row=7,column=i+1).coordinate +":"+ ws.cell(row=ws.max_row-7,column=i+1).coordinate +")"
-                for j in range (1,5):
+                for j in range (1,7):
                     ws.cell(row=ws.max_row-5,column=i-j).value = ""
 
         #Проверка длительности
@@ -96,16 +100,24 @@ for file in os.listdir('.'):
                 for j in range(7,ws.max_row-7):
                     if ws.cell(row=j,column=i).value < 0:
                         print ("ОШИБКА В ДЛИТЕЛЬНОСТИ! В строке ",ws.cell(row=j,column=1).value," отчета")
-                
+
+        for i in range(1,ws.max_column):
+            if ws.cell(row=6,column=i).value == "Идентификатор подписки":
+                j = get_column_letter(i)
+                ws.column_dimensions[j].hidden = True
+
                     
         # Передвину место для подписи
-            
+        ws.cell(row=1, column=10).value = "ОТЧЕТ АГЕНТА"    
         cellLS____ = ws.cell(row=ws.max_row-1, column=6)
         cellMPLS = ws.cell(row=ws.max_row, column=6)
-        ws.cell(row=ws.max_row - 1, column=9).value = cellLS____.value
-        ws.cell(row=ws.max_row, column=9).value = cellMPLS.value
+        GenD = ws.cell(row=ws.max_row-1, column=1)
+        ws.cell(row=ws.max_row , column=9).value = cellLS____.value
+        ws.cell(row=ws.max_row +1, column=9).value = cellMPLS.value
+        ws.cell(row=ws.max_row -1, column=1).value = GenD.value
         cellLS____.value = ''
         cellMPLS.value = ''
+        GenD.value = ''
 
         wb.save(newfilename)
 
